50e851b6d949edb7afd4a95ecf6582339ffcf074
Adrian Aulbach <adrian@aulbach.ch>
2015-02-21 01:44:59 +0100

    Returning elements in the same order as in the key array

--- php/remap_array.php
+++ php/remap_array.php
@@ -6,14 +6,25 @@
  * @param $no_change: If this is true and there is no new key for an element, use the old key. If false the element won't be present in the returned array
  */
 function remap($array, $keys, $no_change=false) {
-	foreach($array as $key => $value) {
-		if(isset($keys[$key])) {
-			//add element with new key to the returned array
-			$result[$keys[$key]] = $value;
-		} elseif($no_change) {
-			//add element with the old key to the returned array
-			$result[$key] = $value;
-		}
-	}
-	return $result;
+    foreach($keys as $keyIn => $keyOut) {
+        $result[$keyOut] = $array[$keyIn];
+        unset($array[$keyIn]);
+    }
+    
+    if($no_change) {
+        return array_merge($result, $array);
+    }
+    
+    return $result;
+    
+//    foreach($array as $key => $value) {
+//		if(isset($keys[$key])) {
+//			//add element with new key to the returned array
+//			$result[$keys[$key]] = $value;
+//		} elseif($no_change) {
+//			//add element with the old key to the returned array
+//			$result[$key] = $value;
+//		}
+//	}
+//	return $result;
 }
