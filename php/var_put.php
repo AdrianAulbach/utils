738d13f0f27c45123c77d8b8f0c1f395628b3d89
Adrian Aulbach <adrian@aulbach.ch>
2015-10-25 18:53:58 +0100

    added var_put

--- php/var_put.php
+++ php/var_put.php
@@ -1,18 +1,19 @@
 <?php
+
 $data = file_get_contents('php://input');
 
-if(preg_match('/(\-+[\w]+)/', $data, $matches)) {
+if(preg_match('/^(\-+[\w]+)/', $data, $matches)) {
     $separator = $matches[1];
     $parts = explode($separator, $data);
 
-    $_PUT = array();
+    $PUT = array();
     foreach($parts as $part) {
-        if(preg_match('/name="([\w]+)"[^\w]*([^\n\r]+)/', $part, $matches)) {
-            $_PUT[$matches[1]] = $matches[2];
+        if(preg_match('/name="(.+)"[^\w]*([^\n\r]+)/', $part, $matches)) {
+            $PUT[$matches[1]] = $matches[2];
         } else {
             //ToDo: error
         }
     }
 } else {
-    //ToDo: error
+    parse_str($data, $PUT);
 }
